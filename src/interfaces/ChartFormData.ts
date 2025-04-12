15cce2a13d60e26494ed15d5535e26707eca344e
Abhinav Goel <abhinavgo1000@gmail.com>
2025-04-12 11:35:33 -0600

    dependencies installd and dummy code removed

---
+++ src/interfaces/ChartFormData.ts
@@ -0,0 +1,18 @@
+export interface ChartFormData {
+    name: string;
+    email: string;
+    telephone: string;
+    dateOfBirth: Date | null;
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+    productName: string;
+    productType: string;
+    productCategory: string;
+    productBrand: string;
+    productPrice: number;
+    productModel: string;
+    productPurchaseDate: Date | null;
+}
