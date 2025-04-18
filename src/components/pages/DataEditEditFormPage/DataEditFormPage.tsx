b0cd671c2411b2118782a9be98c743c5aa7f1a19
Abhinav Goel <abhinavgo1000@gmail.com>
2025-04-18 11:09:28 -0600

    form components imported

--- src/components/pages/DataEditEditFormPage/DataEditFormPage.tsx
+++ src/components/pages/DataEditEditFormPage/DataEditFormPage.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
+import DataEditForm from '../../common/DataEditForm/DataEditForm';
 import './DataEditFormPage.css';
 
 function DataEditFormPage() {
     return (
         <React.Fragment>
-            <Typography variant='h4'>Data Entry Form</Typography>
+            <Typography variant='h4'>Data Edit Form</Typography>
+            <DataEditForm />
         </React.Fragment>
     );
 }
--- src/components/pages/DataEntryFormPage/DataEntryFormPage.tsx
+++ src/components/pages/DataEntryFormPage/DataEntryFormPage.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
+import DataEntryForm from '../../common/DataEntryForm/DataEntryForm';
 import './DataEntryFormPage.css';
 
 function DataEntryFormPage() {
     return (
         <React.Fragment>
             <Typography variant='h4'>Data Entry Form</Typography>
+            <DataEntryForm />
         </React.Fragment>
     );
 }
